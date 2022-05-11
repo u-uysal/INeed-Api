5be82d97fb5f3308e9eea3fcced784757c67b1ec
Ufuk UYSAL <ufuk.uysal@oakfields.io>
2022-05-11 14:39:45 +0200

    lint for better quality

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 const { ApolloServer, gql } = require('apollo-server');
-const mongoose  = require('mongoose');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-const URI = process.env.DB_URI 
+const URI = process.env.DB_URI;
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = gql`
-
   type Query {
     sayHi: String!
   }
 `;
 
 const resolvers = {
-    Query:{
-        sayHi:()=>"hello"
-    }
-}
+  Query: {
+    sayHi: () => 'hello',
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    csrfPrevention: true,
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+});
+
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Database is ok!');
   });
 
-  mongoose.connect(URI,{
-    useNewUrlParser:true
-  }).then(()=>{
-    console.log("Database is ok!")
-  })
-  
-  // The `listen` method launches a web server.
-  server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-  });
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  // eslint-disable-next-line no-console
+  console.log(`🚀  Server ready at ${url}`);
+});
