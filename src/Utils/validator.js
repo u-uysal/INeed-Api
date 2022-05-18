9caae5769d6e88b3bf9d6656e1721667c1a8a9f6
Ufuk UYSAL <ufuk.uysal@oakfields.io>
2022-05-18 22:46:48 +0200

    change copywriting

--- src/Utils/validator.js
+++ src/Utils/validator.js
@@ -2,20 +2,20 @@ import { UserInputError } from 'apollo-server';
 
 const validateRegisterData = (username, email, password, confirmPassword) => {
   if (username.trim() === '') {
-    throw new UserInputError('Username must not be empty');
+    throw new UserInputError('Username should not be empty');
   }
   if (email.trim() === '') {
-    throw new UserInputError('Email must not be empty');
+    throw new UserInputError('Email should not be empty');
   } else {
     const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
     if (!email.match(regEx)) {
-      throw new UserInputError('Email must be a valid email address');
+      throw new UserInputError('Email should be a valid email address');
     }
   }
   if (password === '') {
-    throw new UserInputError('Password must not empty');
+    throw new UserInputError('Password should not empty');
   } else if (password !== confirmPassword) {
-    throw new UserInputError('Passwords must match');
+    throw new UserInputError('Passwords should match');
   }
 };
 export default validateRegisterData;
