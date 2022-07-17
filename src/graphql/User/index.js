92875c8ce9880d0603322d3df9db9d635fcec988
uuysal <Ufuk.Uysal@ingenico.com>
2022-07-17 16:34:58 +0200

    remove comments and rewrite login mutation

--- src/graphql/User/index.js
+++ src/graphql/User/index.js
@@ -26,14 +26,6 @@ export const UserTypeDef = gql`
     email: String!
   }
 
-  # where do you use this?
-  type Login {
-    id: ID!
-    email: String!
-    token: String!
-    createdAt: String!
-  }
-
   type rstPasswort {
     id: ID!
     email: String!
@@ -46,9 +38,13 @@ export const UserTypeDef = gql`
   }
 
   type Mutation {
-    register(data: RegisterInput): User!
-    # if number of arguments is lower than 3, dont need to create an input type
-    # login mutation will always return User type
+    register(
+      firstname: String!
+      lastname: String!
+      password: String!
+      confirmPassword: String!
+      email: String!
+    ): User!
     login(password: String!, email: String!): User!
     resetPasswort(data: resetPasswortData): rstPasswort!
   }
@@ -56,7 +52,7 @@ export const UserTypeDef = gql`
 
 export const UserResolvers = {
   Mutation: {
-    async register(_, { data: { firstname, lastname, password, confirmPassword, email } }) {
+    async register(_, { firstname, lastname, password, confirmPassword, email }) {
       // validate the data
       validateRegisterData(firstname, email, password, confirmPassword);
       // make sure user is not already exists
@@ -85,7 +81,6 @@ export const UserResolvers = {
     async login(_, { email, password }) {
       validateLoginData(email, password);
 
-      // dont need to use email twice. If key and value are same, you can use just key
       const user = await User.findOne({ email });
       if (!user) {
         throw new UserInputError('Invalid credentials');
@@ -98,37 +93,7 @@ export const UserResolvers = {
 
       const token = generateToken(user);
 
-      // never push your secret key. Use environment variable instead.
-
-      // const token = jwt.sign({ id: user.id, email: user.email }, 'somesupersecretkey', {
-      //   expiresIn: '2h',
-      // });
       return { ...user._doc, id: user.id, token };
     },
   },
 };
-// we create a resolver only for the entity. Now the entity is the User since we are in the User folder.
-// Dont need to create another resolver inside the entity.
-// export const LoginResolvers = {
-//   Mutation: {},
-// };
-
-// we create a resolver only for the entity. Now the entity is the User since we are in the User folder.
-// todo create a forgotPasswordResolvers inside the entity.
-
-// export const forgotPasswordResolvers = {
-//   Mutation: {
-//     async resetPasswort(_, { data: { email, password, confirmPassword } }) {
-//       if (password !== confirmPassword) {
-//         throw new Error("Your passwords don't match");
-//       }
-//       const filter = { email };
-//       const update = { password };
-//       const saltRounds = 12;
-//       const hash = await bcrypt.hash(password, saltRounds);
-//       const doc = await User.findOneAndUpdate(filter, update);
-//       if (!doc) throw new Error('Your password reset token is either invalid or expired.');
-//       return doc;
-//     },
-//   },
-// };
